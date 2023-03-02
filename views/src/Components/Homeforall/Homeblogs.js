f7578cc99db655137ca0c7cfe487f8c7f04d98dd
Abhyuday <abhyuday911@gmail.com>
2023-03-03 04:04:01 +0530

    home page save blog > redirect

--- views/src/Components/Homeforall/Homeblogs.js
+++ views/src/Components/Homeforall/Homeblogs.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { asyncloadblogs, asyncloaduser } from "../../store/userActions";
 
 const Homeblogs = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const user = useSelector((state) => state.user);
 
   // console.log(user);
@@ -13,17 +15,27 @@ const Homeblogs = () => {
     dispatch(asyncloaduser());
   }, [dispatch]);
 
+  const homeclickHandler = (params) => {
+    alert("muh me le");
+    navigate("/signup")
+  }
+
   return (
     <>
-      <div id="homeblog">
+      <div id="profileblog">
+        <h2>Trending Stories</h2>
         {user &&
           user.blogs &&
           user.blogs.map((blog) => (
-            <div
-              id="home-card"
-              key={blog._id}
-              dangerouslySetInnerHTML={{ __html: blog.data }}
-            ></div>
+            <div key={blog._id} id="profile-card-parent">
+              <div
+                id="profile-card"
+                dangerouslySetInnerHTML={{ __html: blog.data }}
+              ></div>
+              <section>
+                <i key={blog._id} onClick={homeclickHandler} className="ri-bookmark-line"></i>
+              </section>
+            </div>
           ))}
       </div>
     </>
